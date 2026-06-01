f80cbfc6ec543f92267aefde9ec2692626c30ce3
Natan Oliveira <natanaeloliveira.work22@outlook.com>
2026-06-01 17:05:50 -0400

    Envio de novas vagas por e-mail

--- main.py
+++ main.py
@@ -1,32 +1,109 @@
+import os
+import json
+import smtplib
 import requests
+
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-import os
+from email.mime.text import MIMEText
+
 
 load_dotenv()
 
-url = os.getenv("SITE_URL")
+SITE_URL = os.getenv("SITE_URL")
+EMAIL_REMETENTE = os.getenv("EMAIL_REMETENTE")
+EMAIL_SENHA = os.getenv("EMAIL_SENHA")
+EMAIL_DESTINO = os.getenv("EMAIL_DESTINO")
 
-print("Acessando o site...")
+ARQUIVO_VAGAS = "vagas_encontradas.json"
 
-resposta = requests.get(url)
 
-if resposta.status_code == 200:
-    print("Site acessado com sucesso!") 
-else:
-    print(f"Erro ao acessar o site. Status code: {resposta.status_code}")
-    exit()
+def carregar_vagas_salvas():
+    try:
+        with open(ARQUIVO_VAGAS, "r", encoding="utf-8") as arquivo:
+            return json.load(arquivo)
+    except FileNotFoundError:
+        return []
 
-soup = BeautifulSoup(resposta.text, "html.parser")
 
-links = soup.find_all("a") 
+def salvar_vagas(vagas):
+    with open(ARQUIVO_VAGAS, "w", encoding="utf-8") as arquivo:
+        json.dump(vagas, arquivo, indent=4, ensure_ascii=False)
 
-print("\nVagas de estágio encontradas no site (INDT):\n")
 
-for link in links:
-    texto =  link.get_text(strip=True)
+def enviar_email(titulo, link):
+    print("Enviando e-mail...")
 
-    if "estágio" in texto.lower():
-        print(f"Título: {texto}")
-        print(f"Link: {link.get('href')}")
-        print("-" * 50)
+    mensagem = MIMEText(
+        f"Nova vaga encontrada:\n\nTítulo: {titulo}\nLink: {link}",
+        "plain",
+        "utf-8"
+    )
+
+    mensagem["Subject"] = "Nova vaga de estágio encontrada!"
+    mensagem["From"] = EMAIL_REMETENTE
+    mensagem["To"] = EMAIL_DESTINO
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as servidor:
+        servidor.starttls()
+        servidor.login(EMAIL_REMETENTE, EMAIL_SENHA)
+        servidor.send_message(mensagem)
+
+    print("E-mail enviado com sucesso!")
+
+
+def buscar_vagas():
+    print("Acessando o site...")
+
+    resposta = requests.get(SITE_URL)
+
+    if resposta.status_code != 200:
+        print("Erro ao acessar o site.")
+        return []
+
+    print("Site acessado com sucesso!")
+
+    soup = BeautifulSoup(resposta.text, "html.parser")
+    links = soup.find_all("a")
+
+    vagas = []
+
+    for link in links:
+        titulo = link.get_text(strip=True)
+        url_vaga = link.get("href")
+
+        if "estágio" in titulo.lower():
+            vaga = {
+                "titulo": titulo,
+                "link": url_vaga
+            }
+
+            vagas.append(vaga)
+
+    return vagas
+
+
+def main():
+    vagas_salvas = carregar_vagas_salvas()
+    vagas_encontradas = buscar_vagas()
+
+    links_salvos = []
+
+    for vaga in vagas_salvas:
+        links_salvos.append(vaga["link"])
+
+    for vaga in vagas_encontradas:
+        if vaga["link"] not in links_salvos:
+            print("\nNova vaga encontrada!")
+            print("Título:", vaga["titulo"])
+            print("Link:", vaga["link"])
+
+            enviar_email(vaga["titulo"], vaga["link"])
+            vagas_salvas.append(vaga)
+
+    salvar_vagas(vagas_salvas)
+
+    print("\nVerificação finalizada.")
+
+
+main()
